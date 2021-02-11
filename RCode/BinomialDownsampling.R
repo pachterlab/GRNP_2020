070e68878eb6bc03033df06fb226789a1255cb77
Johan Gustafsson <gustajo@chalmers.se>
2021-02-11 08:59:57 +0100

    Some progress on first revision

---
+++ RCode/BinomialDownsampling.R
@@ -0,0 +1,51 @@
+#before anything else, you need to setup paths:
+#for example
+#source("C:/Work/MatlabCode/projects/HMASandbox/HMA_Sandbox/Butterfly/paths.R")
+#or, in colab:
+#source("GRNP_2020/RCode/pathsGoogleColab.R")
+
+source(paste0(sourcePath, "ButterflyHelpers.R"))
+
+
+library(stringr)
+library(dplyr)
+library(qdapTools)
+
+#t is a vector of downsampling values between 0 and 1
+binomialDownsampling = function(bug, fractionsToKeep) {
+  collapsed = bug %>% group_by(gene) %>% do(countslist=c(.$count))#if you get an error here, you probably defined a variable called "c"...
+  
+  numGenes = dim(collapsed)[1]
+  
+  #create a matrix with genes as rows and fraction to keep values as columns
+  estTotCounts = matrix(data=0,nrow=numGenes,ncol=length(fractionsToKeep))
+  
+  print(paste0("Genes: ",numGenes))
+  
+  
+  for (i in 1:numGenes) {
+    h = hist(collapsed[[2]][[i]], breaks=seq(0.5, max(collapsed[[2]][[i]])+0.5, by=1), plot = F)$counts
+    totCounts = h %*% (1:length(h))
+    lh = length(h)
+    for (j in 1:length(fractionsToKeep)) {
+      ftk = fractionsToKeep[j]
+      hd = rep(0,lh)
+      for (k in 1:lh) {
+        dens = dbinom(1:lh, k, ftk)
+        hd = hd + dens*h[k]
+      }
+      estTotCounts[i,j] = sum(hd)
+    }
+    
+    if ((i %% 1000) == 0) {
+      print(i)
+    }
+  }
+  
+  #annoying conversion, can probably be done smarter
+  nms = c("gene",paste0("d",fractionsToKeep))
+  colnames(estTotCounts) = nms[2:length(nms)]
+  res = bind_cols(tibble(gene=collapsed$gene), as_tibble(estTotCounts))
+  return(res)
+
+}
---
+++ RCode/GenFig5.R
@@ -0,0 +1,241 @@
+#before anything else, you need to setup paths:
+#for example
+#source("C:/Work/MatlabCode/projects/HMASandbox/HMA_Sandbox/Butterfly/paths.R")
+#or, in colab:
+#source("GRNP_2020/RCode/pathsGoogleColab.R")
+
+source(paste0(sourcePath,"ButterflyHelpers.R"))
+
+library(ggplot2)
+library("ggpubr")
+
+source(paste0(sourcePath,"BinomialDownSampling.R"))
+source(paste0(sourcePath,"CCCHelpers.R"))
+
+
+#remember that the genes file is missing in the predquant output folder. Just copy the genes file output by collapse into the folder and rename it.
+
+
+library(BUSpaRse)
+library(textTinyR)
+library(Matrix.utils)
+library(Seurat)
+
+################################################################
+## Use the PBMC_NG + and PBMC_NG_2 datasets and batch correct
+################################################################
+
+#remember that the genes file is missing in the predquant output folder. Just copy the genes file output by collapse into the folder and rename it.
+
+#read NG data
+#############
+NGMatrix = read_count_output(paste0(dataPath, "pbmc5kNextGem/bus_output/genecounts"), "output", tcc=F)
+#filter cells with less than 200 reads
+cs = sparse_Sums(NGMatrix, rowSums = F)
+
+NGMatrix = NGMatrix[,cs > 200]
+
+#convert the genes to gene symbols
+tr2g = read.table(paste0(dataPath, "pbmc5kNextGem/bus_output/transcripts_to_genes.txt"), stringsAsFactors = F)
+lookupTable = tr2g[,2:3]
+lookupTable= unique(lookupTable)
+
+outGenes = lookup(row.names(NGMatrix), lookupTable)
+
+NGMat = NGMatrix
+row.names(NGMat) = outGenes
+NGMat = aggregate.Matrix(NGMat, outGenes, fun='sum')
+
+#Read NG_2 data
+###############
+NG_2Matrix = read_count_output(paste0(dataPath, "pbmc_NG2/bus_output/genecounts"), "output", tcc=F)
+#filter cells with less than 200 reads
+cs = sparse_Sums(NG_2Matrix, rowSums = F)
+
+NG_2Matrix = NG_2Matrix[,cs > 200]
+
+#convert the genes to gene symbols
+tr2g = read.table(paste0(dataPath, "pbmc_NG2/bus_output/transcripts_to_genes.txt"), stringsAsFactors = F)
+lookupTable = tr2g[,2:3]
+lookupTable= unique(lookupTable)
+
+outGenes = lookup(row.names(NG_2Matrix), lookupTable)
+
+NG_2Mat = NG_2Matrix
+row.names(NG_2Mat) = outGenes
+NG_2Mat = aggregate.Matrix(NG_2Mat, outGenes, fun='sum')
+
+#Join the two datasets and process in Seurat
+############################################
+#now, test to merge (inner_join) the two sparse matrices
+mergedmat = merge(NGMat, NG_2Mat, row.names(NGMat), row.names(NG_2Mat), all.x = F, all.y = F)
+
+#verify that the merge seemed to work
+sum(mergedmat[row.names(mergedmat) == "A2M",] == c(NGMat[row.names(NGMat) == "A2M",],NG_2Mat[row.names(NG_2Mat) == "A2M",]))
+dim(mergedmat)#seems to have worked for that gene at least
+
+#now, investigate the matrix in Seurat
+#########################################
+
+set.seed(1)
+
+seurObj = CreateSeuratObject(counts = mergedmat, project = "merged", min.cells = 0, min.features = 0)
+seurObj[["ds_source"]] = factor(as.numeric(c(rep(0, dim(NGMat)[2]), rep(1, dim(NG_2Mat)[2]))), c(0,1), c("PBMC_NG","PBMC_NG_2"))
+seurObj <- NormalizeData(seurObj, normalization.method = "LogNormalize", scale.factor = 10000)
+seurObj <- FindVariableFeatures(seurObj, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(seurObj)
+seurObj <- ScaleData(seurObj, features = all.genes)
+seurObj <- RunPCA(seurObj, features = VariableFeatures(object = seurObj))
+seurObj <- FindNeighbors(seurObj, dims = 1:10)
+seurObj <- FindClusters(seurObj, resolution = 0.5)
+seurObj <- RunUMAP(seurObj, dims = 1:10)
+#DimPlot(seurObj, reduction = "umap", group.by = "ds_source", cols=c("blue", "red"))
+
+FeaturePlot(seurObj, features = c("CD19", "CD8A", "CD3D")) # The clusters are T cells
+
+supFigA = DimPlot(seurObj, reduction = "umap")
+
+ggsave(
+  paste0(figure_path, "FigS_batch.png"),
+  plot = supFigA, device = "png",
+  width = 8, height = 5, dpi = 300)
+
+
+
+#seurObj2 = subset(seurObj, subset = seurat_clusters == 0 | seurat_clusters == 3 | seurat_clusters == 5 | seurat_clusters == 6) 
+#seurObj2 = subset(seurObj, subset = seurat_clusters == 0) 
+#DimPlot(seurObj2, reduction = "umap")
+
+clusters = seurObj$seurat_clusters
+
+subsel = clusters == 0 | clusters == 3 | clusters == 5 | clusters == 6
+#subsel = seurObj$seurat_clusters == 0
+dsSource = seurObj$ds_source[subsel]
+
+#now the reduced set
+
+mergedmatsub = mergedmat[,subsel]
+
+seurObj = CreateSeuratObject(counts = mergedmatsub, project = "merged", min.cells = 0, min.features = 0)
+seurObj[["percent.mt"]] <- PercentageFeatureSet(seurObj, pattern = "^MT-")
+seurObj[["ds_source"]] <- dsSource
+
+seurObj <- NormalizeData(seurObj, normalization.method = "LogNormalize", scale.factor = 10000)
+seurObj <- FindVariableFeatures(seurObj, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(seurObj)
+seurObj <- ScaleData(seurObj, features = all.genes)
+seurObj <- RunPCA(seurObj, features = VariableFeatures(object = seurObj))
+seurObj <- FindNeighbors(seurObj, dims = 1:10)
+seurObj <- FindClusters(seurObj, resolution = 0.5)
+seurObj <- RunUMAP(seurObj, dims = 1:10)
+pA = DimPlot(seurObj, reduction = "umap", group.by = "ds_source", cols=c("blue", "red"))
+pA = pA + ggtitle("Uncorrected")
+pA = pA +   theme(panel.background = element_rect("white", "white", 0, 0, "white"),
+                  legend.position= "bottom", legend.direction = "horizontal",#, legend.title = element_blank())
+                  strip.text.x = element_text(size = 12, face = "bold"),
+                  #legend.position= "none",
+                  strip.background = element_blank())
+
+pA
+
+#Now, correct by down-sampling NG2 to 0.49
+##############################
+
+loadStats("PBMC_NG_2")
+
+dsc = statsPBMC_NG_2[,c(1, which(colnames(statsPBMC_NG_2) == "CPM_PBMC_NG_2_d_40"), which(colnames(statsPBMC_NG_2) == "CPM_PBMC_NG_2_d_100"))]
+
+#so, test to use binomial downsampling
+loadBug("PBMC_NG_2")
+bugvNg2 = getBug("PBMC_NG_2")
+
+dsManyMany = binomialDownsampling(bugvNg2, c(0.45,0.46,0.47,0.48,0.49,0.50,0.51,0.52,0.53,0.54,0.55))
+
+dsMany = dsManyMany[,c(1,6)] #0.49 maximizes the correlation
+dsManyCPM = dsMany
+mergeMany = inner_join(dsManyCPM, dsc, by="gene")
+
+#recalculate to CPM
+for (i in 2:4) {
+  mergeMany[[i]] = mergeMany[[i]] * 10^6/sum(mergeMany[[i]])
+}
+
+scale = mergeMany[[2]] / mergeMany[[4]]
+NG_2mod = NG_2Mat[row.names(NG_2Mat) %in% mergeMany$gene, ]
+mat = match(mergeMany$gene, row.names(NG_2mod))
+sum(is.na(mat))#0, good
+
+scaled_NG_2 = NG_2mod * scale[mat];
+numNG2 = dim(scaled_NG_2)[2]
+
+mergedmatX = merge(NGMat, scaled_NG_2, row.names(NGMat), row.names(scaled_NG_2), all.x = F, all.y = F)
+
+cpmNG2 = sparse_Sums(mergedmatX[, 1:numNG2], T)
+cpmNG = sparse_Sums(mergedmatX[, (numNG2+1):(dim(mergedmatX)[2])], T)
+
+cpmNG2 = cpmNG2 *10^6/sum(cpmNG2)
+cpmNG = cpmNG *10^6/sum(cpmNG)
+
+cpmNG2l = log2(cpmNG2 + 1)
+cpmNGl = log2(cpmNG + 1)
+
+getCCC(cpmNG2l, cpmNGl) # 0.9935431
+
+#compare to original
+mergedmat2 = merge(NG_2mod, NGMat,  row.names(NG_2mod), row.names(NGMat),all.x = F, all.y = F)
+
+cpmNG2__2 = sparse_Sums(mergedmat2[, 1:numNG2], T)
+cpmNG__2 = sparse_Sums(mergedmat2[, (numNG2+1):(dim(mergedmat2)[2])], T)
+
+cpmNG2__2 = cpmNG2__2 *10^6/sum(cpmNG2__2)
+cpmNG__2 = cpmNG__2 *10^6/sum(cpmNG__2)
+
+cpmNG2__2l = log2(cpmNG2__2 + 1)
+cpmNG__2l = log2(cpmNG__2 + 1)
+
+getCCC(cpmNG2__2l, cpmNG__2l) # 0.9904393
+
+mergedmatsub = mergedmatX[,subsel]
+
+seurObj = CreateSeuratObject(counts = mergedmatsub, project = "merged", min.cells = 0, min.features = 0)
+#seurObj[["percent.mt"]] <- PercentageFeatureSet(seurObj, pattern = "^MT-")
+seurObj[["ds_source"]] <- dsSource
+seurObj <- NormalizeData(seurObj, normalization.method = "LogNormalize", scale.factor = 10000)
+seurObj <- FindVariableFeatures(seurObj, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(seurObj)
+seurObj <- ScaleData(seurObj, features = all.genes)
+#seurObj = SCTransform(seurObj, vars.to.regress = "percent.mt", verbose = TRUE)
+seurObj <- RunPCA(seurObj, features = VariableFeatures(object = seurObj))
+seurObj <- FindNeighbors(seurObj, dims = 1:10)
+seurObj <- FindClusters(seurObj, resolution = 0.5)
+seurObj <- RunUMAP(seurObj, dims = 1:10)
+pB = DimPlot(seurObj, reduction = "umap", group.by = "ds_source", cols=c("blue", "red"))
+pB = pB + ggtitle("Corrected")
+pB = pB +   theme(panel.background = element_rect("white", "white", 0, 0, "white"),
+                  legend.position= "bottom", legend.direction = "horizontal",#, legend.title = element_blank())
+                  strip.text.x = element_text(size = 12, face = "bold"),
+                  #legend.position= "none",
+                  strip.background = element_blank())
+
+pB
+
+fig5 = ggarrange(pA, pB, nrow=1, ncol=2, labels=c("A","B"))
+
+ggsave(
+  paste0(figure_path, "Fig5.png"),
+  plot = fig5, device = "png",
+  width = 8, height = 5, dpi = 300)
+
+
+
+
+#################
+# test the binomial downsampling function
+#################
+
+#loadBug("PBMC_V3_3")
+#bugv3_3 = getBug("PBMC_V3_3")
+#bug = bugv3_3
+#t = 0.1
+#ds = binomialDownsampling(bug, c(0.1,0.5))
+
