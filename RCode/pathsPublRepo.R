c89e6ed3a8253b093252e05a05834e1e4eb5822c
Johan Gustafsson <gustajo@chalmers.se>
2021-03-01 12:39:09 +0100

    Added tr2g for notebooks + minor changes

---
+++ RCode/pathsPublRepo.R
@@ -0,0 +1,5 @@
+sourcePath = "C:/Work/R/ButterflyPublicationRepo/GRNP_2020/RCode/"
+dataPath = "E:/Butterfly/"
+figure_data_path = "C:/Work/MatlabCode/projects/HMASandbox/HMA_Sandbox/Butterfly/FigureData/"
+figure_path = "Z:/projects/Butterfly/figures/"
+CONSERVE_MEM = F
